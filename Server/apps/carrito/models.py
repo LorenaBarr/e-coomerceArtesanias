c3368e48818806b7ec261b31baf43a2dfa90e2ff
Samsa Ahuerma <106261599+SamsaAhuerma@users.noreply.github.com>
2024-04-07 23:29:19 -0300

    Add files via upload
    Subiendo app carrito

---
+++ Server/apps/carrito/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Carrito, Producto
+
+def mostrar_carrito(request):
+    # Lógica para obtener y mostrar el contenido del carrito
+    return render(request, 'carrito.html', {'contenido_carrito': contenido_carrito})
+
+def agregar_al_carrito(request, producto_id):
+    # Lógica para agregar el producto al carrito
+    return render('mostrar_carrito')
+
+def actualizar_carrito(request, producto_id):
+    # Lógica para actualizar la cantidad de un producto en el carrito
+    return render('mostrar_carrito')
+
+def eliminar_del_carrito(request, producto_id):
+    # Lógica para eliminar un producto del carrito
+    return render('mostrar_carrito')
+
+def procesar_pago(request):
+    # Lógica para procesar el pago y finalizar la compra
+    return render('pagina_confirmacion_pago')
