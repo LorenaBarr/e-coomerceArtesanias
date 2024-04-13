4f5f204e5d8019484392d33a797e488af740a13e
Lorena Barrera <karenlbarrera@gmail.com>
2024-04-12 20:07:27 -0500

    nav menu

--- Client/src/Components/Nabvar/Menu.jsx
+++ Client/src/Components/Nabvar/Menu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Menu = () => {
     return(
-<ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+      <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box flex justify-center">
   <li><a>Inicio</a></li>
   <li><a>Nosotros</a></li>
   <li><a>Productos</a></li>
