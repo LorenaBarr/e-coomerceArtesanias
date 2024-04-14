69c46c83243921c3c8a1a0bf2e94052de5f5ea91
cometas321 <renerolando321@gmail.com>
2024-04-13 21:30:37 -0500

    ya casi termino con users

--- Server/apps/cart/models.py
+++ Server/apps/cart/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 # Create your models here.
 class shoppingCart(models.Model):
-    orderID = models.AutoField(primary_key=True)
     total = models.DecimalField(max_digits=10, decimal_places=2)
     state = models.CharField(max_length=50)
-    product = models.ForeignKey('product.Product', on_delete=models.CASCADE)
-    # user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+
 
     def __str__(self):
         return str(self.orderID)
