83be6a2839cea9895f8ca789a787df6f08c1645b
cometas321 <renerolando321@gmail.com>
2024-04-16 23:08:00 -0500

    app user hechooo

--- Server/core/settings.py
+++ Server/core/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'America/Lima'
+TIME_ZONE = 'America/Lima' 
 USE_I18N = True
 USE_TZ = True
 
