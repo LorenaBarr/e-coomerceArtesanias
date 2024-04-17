fae4f5770d150ecf351c68a6059e77f1de8ceae9
cometas321 <renerolando321@gmail.com>
2024-04-16 22:31:05 -0500

    app user listo

--- Server/core/settings.py
+++ Server/core/settings.py
@@ -109,12 +109,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'es' #lenguaje en ingles 
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'America/Lima'
 USE_I18N = True
-USE_I10N = True
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
