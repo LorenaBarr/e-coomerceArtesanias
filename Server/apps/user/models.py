438df16785eb8ffb0ecc9737df1de39287b44c82
cometas321 <renerolando321@gmail.com>
2024-04-16 22:07:31 -0500

    app user listo

--- Server/apps/user/models.py
+++ Server/apps/user/models.py
@@ -1,41 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
 
-class UserAccountManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Los usuarios deben tener una dirección de correo electrónico')
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        user = self.create_user(email, password, **extra_fields)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
-
-        return user
-
-class User(AbstractUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True)
-
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    is_editor = models.BooleanField(default=False)
-
-    objects = UserAccountManager()
+class User(models.Model):
+    Name = models.CharField(max_length=50, default='')
+    LastName = models.CharField(max_length=50, default='')
+    Email = models.CharField(max_length=50, default='')
+    Password = models.CharField(max_length=255, default='')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-    
-    def __str__(self):
-        return self.email
+    REQUIRED_FIELDS = ['Email', 'Password']
+
+    def to_dict(self):
+        return {
+            'Name': self.Name,
+            'LastName': self.LastName,
+            'Email': self.Email,
+            'Password': self.Password,
+        }
