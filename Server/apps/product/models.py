73acb036beb73ff62702babc0cdc385a7932aed6
Rene Rolando <149093692+cometas321@users.noreply.github.com>
2024-04-23 16:25:16 -0500

    Update models.py

--- Server/apps/product/models.py
+++ Server/apps/product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    URL = models.URLField()
+    URL = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE)
 
