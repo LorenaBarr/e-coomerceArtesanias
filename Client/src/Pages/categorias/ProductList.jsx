048fa27fb985244ff9af2573d8e8aa54274b0cc5
Victor Raul <v1c70rotakmer@gmail.com>
2024-04-25 23:38:44 -0500

    Refactor ProductList component in ProductList.jsx

--- Client/src/Pages/categorias/ProductList.jsx
+++ Client/src/Pages/categorias/ProductList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import { useState, useEffect } from "react";
 // import ProductModal from "../../Components/Card/ProductModal";
